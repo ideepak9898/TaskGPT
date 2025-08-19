141dd02881f584f69404cfc8e69c6c9fd2279d82
ideepak9898 <deepakraj33333@gmail.com>
2025-08-20 00:54:01 +0530

    backend commit

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,3 +1,8 @@
-export const API_URL = "https://task-gpt-server.vercel.app/";
+// export const API_URL = ["https://taskgpt-jqur.onrender.com/"];
 
 // 'http://localhost:5050'
+
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://taskgpt-jqur.onrender.com/"
+    : "http://localhost:5050";
