10537cc4d7a7546ef8c8a2b4000c3890fd1108d5
ideepak9898 <deepakraj33333@gmail.com>
2025-08-20 01:03:39 +0530

    backend commit

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,7 +1,3 @@
-// export const API_URL = ["https://taskgpt-jqur.onrender.com/"];
-
-// 'http://localhost:5050'
-
 export const API_URL =
   process.env.NODE_ENV === "production"
     ? "https://taskgpt-jqur.onrender.com"
