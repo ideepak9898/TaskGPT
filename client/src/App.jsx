42e9acd329bac653bdc079a01fa55c72517a2f14
ideepak9898 <deepakraj33333@gmail.com>
2025-08-22 04:15:34 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import AuthHome from "./components/AuthHome";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
