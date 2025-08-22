426366f15dc4e9d77f8f69425d5e45168d120e26
ideepak9898 <deepakraj33333@gmail.com>
2025-08-22 15:55:10 +0530

    change backend

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,9 +1,9 @@
 export const API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://taskgpt-jqur.onrender.com"
+    ? "https://task-gpt-backend.vercel.app"
     : "http://localhost:5050";
 
 export const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://taskgpt-jqur.onrender.com/api"
+    ? "https://task-gpt-backend.vercel.app/api"
     : "http://localhost:5050/api";
