78042163606714fe906e8e3bc6580b52768d578c
ideepak9898 <deepakraj33333@gmail.com>
2025-08-22 15:46:59 +0530

    fix GET error

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 const { signup, login } = require("../controllers/authController");
 
+// GET endpoint for /api/auth
+router.get("/", (req, res) => {
+  res.json({ message: "Auth routes are working" });
+});
+
 // Match frontend calls
 router.post("/register", signup);
 router.post("/login", login);
